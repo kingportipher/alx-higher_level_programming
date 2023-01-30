9c10ac607476247278ead140553cfc7af707e5c0
Victor Abrokwah <kingportipher@outlook.com>
2023-01-30 09:41:42 -0800

    Task 2 fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -37,6 +37,28 @@ def Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
 
+        self.__width = valuei
+
+    @property
+    def height(self):
+        """Getter function to height
+        Returns:
+        int -- height of the rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter function os height
+        Arguments:
+        TypeError: height must be an integer
+        ValueError: height must be >= 0
+        """
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
         self.__height = value
 
     def area(self):
