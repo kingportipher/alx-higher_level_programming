5e2680e7d3aee056fdb8180403d6f78a52cef6e9
Victor Abrokwah <kingportipher@outlook.com>
2023-02-06 10:31:04 -0800

    TAsk 6 done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry"""
+
+
+class BaseGeometry:
+    """A class with area method"""
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implementd")
