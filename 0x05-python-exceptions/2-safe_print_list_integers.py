40215c0f937626a0d10e13e77818db7d3f0b6f6a
Victor Abrokwah <kingportipher@outlook.com>
2024-02-05 09:12:12 -0800

    Task 2 refix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            # Attempt to access the element at index i
             try:
-                # Try to convert the element to an integer
-                num = int(my_list[i])
-                # If successful, print the integer
-                print("{:d}".format(num), end=' ')
-                count += 1
-            except (ValueError, TypeError):
-                # If conversion fails, skip the element silently
-                pass
-        # Print a new line after printing all integers
-        print()
-    except IndexError:
-        # Handle the case when x is greater than the length of my_list
-        print("Exception: x is greater than the length of my_list")
+                if isinstance(my_list[i], int):
+                    print("{:d}".format(my_list[i]), end=' ')
+                    count += 1
+            except IndexError:
+                # Handle the case when x is greater than the length of my_list
+                raise Exception("Exception: x is greater than the length of my_list")
+    except Exception as e:
+        print(e)
+    print()
     return count
 
 # Example usage:
 my_list = [1, 'a', 2, 'b', 3, 4, 'c']
 x = 6
 print("Number of integers printed:", safe_print_list_integers(my_list, x))
+
