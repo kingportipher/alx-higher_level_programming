ea7dc7aa2e624c94c31dacbba3cd911f6ab0b933
Victor Abrokwah <kingportipher@outlook.com>
2024-02-19 09:15:39 -0800

    Task done

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""lookup"""
+
+
+def lookup(obj):
+    """function that returns the list of available '\n'
+    attributes and methods of an object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""MyList class"""
+
+
+class MyList(list):
+    """A class MyList that inherits from list"""
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
