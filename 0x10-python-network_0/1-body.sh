364ec30327a520f0d780f9c7658343e80afa7d6b
Victor Abrokwah <kingportipher@outlook.com>
2024-05-13 09:20:05 -0700

    Task 1 fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
-#!/usr/bash
-#a Bash script that takes in a URL, sends a GET request to the URL, 
-#and displays the body of the response
-curl -sX GET "$1" -L 200
+#!/bin/bash
+# Sends a GET request to the URL, and displays the body of the response
+curl -s -L "${1}"
