7695db13c662d8b18598a1aec5b649ffd0d06fd2
Victor Abrokwah <kingportipher@outlook.com>
2024-05-13 09:18:03 -0700

    Task 0 refix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,25 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# URL provided as argument
-URL=$1
-
-# Send request to URL and store response body in a temporary file
-response=$(curl -s -w "%{size_download}" -o /tmp/response_body "$URL")
-
-# Check if curl command was successful
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to fetch data from $URL"
-    exit 1
-fi
-
-# Display the size of the response body in bytes
-echo "Size of the response body: $response bytes"
-
-# Clean up temporary file
-rm /tmp/response_body
+# Sends a request to that URL, and displays the size of the body of the response
+curl -s "${1}" | wc -c
