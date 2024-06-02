872def2a83e7ce26b22af1710f683f9734d2c3c8
Victor Abrokwah <kingportipher@outlook.com>
2024-06-02 15:34:03 -0700

    Task 3 done

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Sends a request to the URL and displays the body
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    from urllib import request, error
+
+    argv = sys.argv
+    url = argv[1]
+    try:
+        with request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as err:
+        print("Error code: {}".format(err.status))
