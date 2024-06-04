082fb0f670d88e7ca399f6c78cf5036cd15a7bf5
Victor Abrokwah <kingportipher@outlook.com>
2024-06-04 16:22:25 -0700

    Task 1 fix

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,23 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
-});
+
+function writeToFile(filePath, content) {
+  fs.writeFile(filePath, content, 'utf8', (err) => {
+    if (err) {
+      console.error("Error writing to file:", err);
+    } else {
+      console.log("File written successfully!");
+    }
+  });
+}
+
+// Example usage (assuming Node.js environment)
+if (process.argv.length !== 3) {
+  console.error("Usage: node writeToFile.js <filepath> <string>");
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+writeToFile(filePath, content);
