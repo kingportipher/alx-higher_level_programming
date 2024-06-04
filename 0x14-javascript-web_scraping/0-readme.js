bf49cd7696d15318e1ce62a102c443dabcc1f753
Victor Abrokwah <kingportipher@outlook.com>
2024-06-04 16:03:28 -0700

    Task 0 done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-const process = require('process');
-
-// Retrieve the file path from the command line arguments
-const filePath = process.argv[2];
-
-// Read the file content in utf-8 encoding
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        // Print the error object if an error occurred
-        console.error(err);
-    } else {
-        // Print the content of the file
-        console.log(data);
-    }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
