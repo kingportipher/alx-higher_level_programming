47ffc579b30a7cf893c7497276c9f6bb32f70f87
Victor Abrokwah <kingportipher@outlook.com>
2024-06-04 15:47:11 -0700

    Task 0 fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,20 @@
 #!/usr/bin/node
-// Read from file
 
-const filesys = require('fs');
-filesys.readFile(process.argv[2], 'utf-8',
-  function (err, data) {
+#!/usr/bin/env node
+
+const fs = require('fs');
+const process = require('process');
+
+// Retrieve the file path from the command line arguments
+const filePath = process.argv[2];
+
+// Read the file content in utf-8 encoding
+fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      console.log(err);
-      return;
+        // Print the error object if an error occurred
+        console.error(err);
+    } else {
+        // Print the content of the file
+        console.log(data);
     }
-    console.log(data);
-  });
+});
